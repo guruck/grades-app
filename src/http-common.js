2849dbb0abe5b4a21880d3327dfa35e226aee4a4
guruck <guruck@gmail.com>
2020-07-04 15:27:01 -0300

    ajustadoServicos

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  // baseURL: 'http://177.189.98.6/',
+  baseURL: 'https://dazzling-kenai-fjords-13206.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
